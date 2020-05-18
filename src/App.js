797a7c63361366e4e06d6310c18b551592d6a80e
mahathi srivatsa <mahathi.srivatsa@hashedin.com>
2020-05-18 22:04:49 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,223 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import React, { useState } from "react";
+import {
+  ListIcon,
+  DeleteIcon,
+  Checkedcircle,
+  UndoIcon,
+  RadioIcon,
+  AddIcon
+} from "./Icons";
+import "./index.css";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const [finalList, setTodoList] = useState([]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let todos = [
+      ...finalList,
+      {
+        task: event.target.task.value,
+        completed: false
+      }
+    ];
+    setTodoList(todos);
+    setTask("");
+  }
+
+  function handleChange(event) {
+    setTask(event.target.value);
+  }
+
+  function handleCheckboxChange(event) {
+    event.completed = !event.completed;
+    setClicked(!clicked);
+  }
+
+  function handleDelete(item) {
+    let index = finalList.indexOf(item);
+    finalList.splice(index, 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <React.Fragment>
+      <Header />
+      <div
+        css={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "rgba(229, 229, 229, 0.57)",
+          borderRadius: "37px",
+          height: "3.2em",
+          marginLeft: "10%",
+          marginRight: "10%",
+          marginTop: "1.5em"
+        }}
+      >
+        <form
+          onSubmit={handleSubmit}
+          css={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            padding: "5px",
+            width: "100%"
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <span
+            css={{
+              fontSize: "24px",
+              color: "#7B20C4"
+            }}
+          >
+            Create New Task
+          </span>
+          <input
+            type="input"
+            name="task"
+            onChange={handleChange}
+            value={task}
+            css={{
+              boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+              borderRadius: "37px",
+              width: "60%",
+              padding: "3px 20px",
+              boxSizing: "border-box",
+              fontSize: "18px",
+              color: "#7B20C4",
+              borderStyle: "none"
+            }}
+            placeholder="Enter your new task here..."
+          />
+          <AddIcon />
+        </form>
+      </div>
+      <ProgressBar finalList={finalList} />
+      <div
+        css={{
+          background: "rgba(229, 229, 229, 0.57)",
+          borderRadius: "37px",
+          width: "90%",
+          minHeight: "30em",
+          marginLeft: "5%",
+          marginRight: "5%",
+          marginTop: "1.5em",
+          paddingTop: "1em",
+          paddingbottom: "1em"
+        }}
+      >
+        {finalList.length > 0 &&
+          finalList.map((item, i) => (
+            <div
+              key={i}
+              onClick={() => handleCheckboxChange(item)}
+              css={{
+                background: "#ffffff",
+                minHeight: "3em",
+                width: "90%",
+                borderRadius: "10px",
+                marginLeft: "5%",
+                marginRight: "5%",
+                marginBottom: "1em",
+                display: "grid",
+                gridTemplateColumns: "5% 80% 5% 5%",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              {item.completed ? <Checkedcircle /> : <RadioIcon />}
+              <span
+                css={{
+                  color: "#7B20C4",
+                  fontSize: "24px",
+                  textDecoration: item.completed ? "line-through" : ""
+                }}
+              >
+                {item.task}
+              </span>
+              {item.completed ? <UndoIcon /> : <Checkedcircle />}
+              <span onClick={() => handleDelete(item)}>
+                <DeleteIcon />
+              </span>
+            </div>
+          ))}
+      </div>
+    </React.Fragment>
   );
 }
+
+const Header = () => {
+  return (
+    <div
+      css={{
+        background:
+          "linear-gradient(180deg, rgba(123, 32, 196, 0.33) 0%, rgba(255, 255, 255, 0.33) 100%)",
+        fontWeight: "bold",
+        fontSize: "35px",
+        lineHeight: "53px",
+        display: "flex",
+        justifyContent: "center",
+        color: "#ffffff",
+        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)"
+      }}
+    >
+      To Do list
+      <ListIcon />
+    </div>
+  );
+};
+
+const ProgressBar = finalList => {
+  let completedListLength = finalList.finalList.filter(
+    item => item.completed === true
+  ).length;
+  let innerWidth = (completedListLength / finalList.finalList.length) * 100;
+  return (
+    <div
+      css={{
+        background: "rgba(229, 229, 229, 0.57)",
+        borderRadius: "37px",
+        display: "flex",
+        height: "3em",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "2em",
+        width: "80%",
+        marginLeft: "10%",
+        fontSize: "18px",
+        color: "#7B20C4"
+      }}
+    >
+      <span>Progress Tracker </span>
+      <div css={{ width: "80%", height: "50%", padding: "0.5em" }}>
+        <div
+          css={{
+            background: "#ffffff",
+            width: "100%",
+            height: "100%",
+            borderRadius: "37px"
+          }}
+        >
+          <div
+            css={{
+              background: "rgba(123, 32, 196, 0.33)",
+              width: innerWidth ? `${innerWidth}%` : "0%",
+              height: "100%",
+              borderRadius: "37px"
+            }}
+          ></div>
+        </div>
+      </div>
+      <span>
+        {completedListLength}/{finalList.finalList.length}
+      </span>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/Icons.js
@@ -0,0 +1,95 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+
+export const ListIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="53"
+      viewBox="0 0 24 24"
+      width="50"
+    >
+      <path
+        d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z"
+        fill="#ffffff"
+      />
+      <path d="M0 0h24v24H0z" fill="none" />
+    </svg>
+  );
+};
+
+export const AddIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="34"
+      viewBox="0 0 24 24"
+      width="34"
+      fill="rgba(123, 32, 196, 0.33)"
+      css={{ cursor: "pointer" }}
+    >
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z" />
+    </svg>
+  );
+};
+
+export const RadioIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="25"
+      viewBox="0 0 24 24"
+      width="25"
+      fill="#7B20C4"
+    >
+      <path d="M12 7c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zm0-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z" />
+      <path d="M0 0h24v24H0z" fill="none" />
+    </svg>
+  );
+};
+
+export const Checkedcircle = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="25"
+      viewBox="0 0 24 24"
+      width="25"
+      fill="#238121"
+    >
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4 8-8z" />
+    </svg>
+  );
+};
+
+export const DeleteIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="25"
+      viewBox="0 0 24 24"
+      width="25"
+      fill="#C42A20"
+    >
+      <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+      <path d="M0 0h24v24H0z" fill="none" />
+    </svg>
+  );
+};
+
+export const UndoIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="25"
+      viewBox="0 0 24 24"
+      width="25"
+      fill="#7B20C4"
+    >
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path d="M12.5 8c-2.65 0-5.05.99-6.9 2.6L2 7v9h9l-3.62-3.62c1.39-1.16 3.16-1.88 5.12-1.88 3.54 0 6.55 2.31 7.6 5.5l2.37-.78C21.08 11.03 17.15 8 12.5 8z" />
+    </svg>
+  );
+};
